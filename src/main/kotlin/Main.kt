b484b879472b9f66cfe148ebf71084601a12490c
melli <melchiorG@gMail.com>
2022-05-15 23:34:18 +0200

    competition.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+
+fun main() {
+    val tournament = Server()
+    tournament.addPrisoner(Good)
+    tournament.addPrisoner(Evil)
+    tournament.addPrisoner(TitForTat)
+    tournament.addPrisoner(TitForTat)
+    tournament.addPrisoner(RandomPrisoner())
+    tournament.addPrisoner(PatternPrisoner(listOf(Prisoner.Choice.COOPERATE, Prisoner.Choice.TRICK, Prisoner.Choice.COOPERATE)))
+    tournament.addPrisoner(PatternPrisoner(listOf(Prisoner.Choice.COOPERATE, Prisoner.Choice.TRICK, Prisoner.Choice.TRICK, Prisoner.Choice.COOPERATE)))
+    tournament.addPrisoner(PatternPrisoner(listOf(Prisoner.Choice.COOPERATE, Prisoner.Choice.TRICK, Prisoner.Choice.COOPERATE, Prisoner.Choice.TRICK, Prisoner.Choice.COOPERATE)))
+    tournament.addPrisoner(SlowlyAdjustingPrisoner())
+    tournament.addPrisoner(SlowlyAdjustingPrisoner())
+    println(tournament.getPrisoners().joinToString())
+
+    tournament.playTournament()
+    tournament.printResults()
+}
