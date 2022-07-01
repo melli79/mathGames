9997d470fcc2ed6936f3f69f7c3e824e9978190e
melli <melchiorG@gMail.com>
2022-07-01 10:43:16 +0200

    Ransom note.

---
+++ src/main/kotlin/ransomnote/ransomnote.kt
@@ -0,0 +1,25 @@
+package ransomnote
+
+fun ransomNote(magazine :String, message :String) :List<Int>? {
+    val result = mutableListOf<Int>()
+    val input = magazine.iterator(); var i=0
+    val filter = message.iterator()
+    while (input.hasNext()&&filter.hasNext()) {
+        var c = filter.next()
+        while (input.hasNext()) {
+            val t = input.next()
+            if (c!=t)
+                i++
+            else {
+                result.add(i++)
+                c = 0.toChar()
+                break
+            }
+        }
+        if (c!=0.toChar())
+            return null
+    }
+    if (filter.hasNext())
+        return null
+    return result
+}
