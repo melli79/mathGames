0eee9197b0bfd145dc2d2554210a70b47bfe1ee7
melli <melchiorG@gMail.com>
2022-07-02 12:02:54 +0200

    cascade filler minimum number of pipes.

---
+++ src/test/kotlin/cascade/MinFillTimeTester.kt
@@ -0,0 +1,40 @@
+package cascade
+
+import java.lang.System.currentTimeMillis
+import kotlin.random.Random
+import kotlin.system.measureNanoTime
+import kotlin.test.*
+
+class MinFillTimeTester {
+    @Test fun emptyCascade() {
+        assertEquals(0u, computeMinFillTime(emptyList()))
+    }
+
+    @Test fun singleBasin() {
+        assertEquals(1u, computeMinFillTime(listOf(1u)))
+    }
+
+    @Test fun lowerFollowing() {
+        assertEquals(2u, computeMinFillTime(listOf(2u, 1u)))
+    }
+
+    @Test fun jointFilling() {
+        assertEquals(2u, computeMinFillTime(listOf(1u, 3u)))
+    }
+
+    @Test fun multipleJointFilling() {
+        assertEquals(3u, computeMinFillTime(listOf(1u, 2u, 4u)))
+    }
+
+    val random = Random(currentTimeMillis())
+
+    @Test fun performance() {
+        assertTrue((1..10).map {
+            val volumes = (1..200_000).map { random.nextInt().toUInt() }
+            val duration = measureNanoTime {
+                assertTrue(computeMinFillTime(volumes) <= volumes.max())
+            }
+            duration <= 18_000_000
+        }.count { !it } <= 1)
+    }
+}
