1ebaed28eea78e48b5eaffe9269629237fb9a7ba
melli <melchiorG@gMail.com>
2022-11-07 23:50:22 +0100

    discrete Nash equilibrium (case 2 and 3 players).

---
+++ src/test/kotlin/equilibrium/Nash2Tester.kt
@@ -0,0 +1,34 @@
+package equilibrium
+
+import kotlin.test.*
+
+class Nash2Tester {
+    @Test fun empty() {
+        val (r1, r2) = nash2(emptyList(), emptyList())
+        assertTrue(r1.isEmpty())
+        assertTrue(r2.isEmpty())
+    }
+
+    @Test fun onePair() {
+        val input = listOf(1, 2)
+        val (r1, r2) = nash2(input, emptyList())
+        assertEquals(r1.size, r2.size)
+        assertEquals(input.toSet(), (r1+r2).toSet())
+        assertEquals(listOf(1), r1)
+    }
+
+    @Test fun oddLength() {
+        val input = listOf(1,2,3,4,5)
+        val (r1, r2) = nash2(input, emptyList())
+        assertEquals(r2.size+1, r1.size)
+        assertEquals(input.toSet(), (r1+r2).toSet())
+    }
+
+    @Test fun threePairs() {
+        val input = listOf(1, 2, 3, 4, 5, 6)
+        val (r1, r2) = nash2(input, listOf(2,3,4,5,6,1))
+        assertEquals(r1.size, r2.size)
+        assertEquals(input.toSet(), (r1+r2).toSet())
+        assertEquals(listOf(2, 4, 1), (r1))
+    }
+}
---
+++ src/test/kotlin/equilibrium/Nash3Tester.kt
@@ -0,0 +1,40 @@
+package equilibrium
+
+import kotlin.test.*
+
+class Nash3Tester {
+    @Test fun empty() {
+        val (r1, r2, r3) = nash3(emptyList(), emptyList(), emptyList())
+        assertTrue(r1.isEmpty())
+        assertTrue(r2.isEmpty())
+        assertTrue(r3.isEmpty())
+    }
+
+    @Test fun oneEach() {
+        val input = listOf(1, 2, 3)
+        val (r1, r2, r3) = nash3(input, input, listOf(3,1,2))
+        assertEquals(r1.size, r2.size)
+        assertEquals(r1.size, r3.size)
+        assertEquals(input.toSet(), (r1+r2+r3).toSet())
+        assertEquals(listOf(1), r1)
+        assertEquals(listOf(2), r2)
+    }
+
+    @Test fun oneTooFew() {
+        val input = listOf(1, 2)
+        val (r1, r2, r3) = nash3(input, input, input)
+        assertEquals(r1.size, r2.size)
+        assertEquals(r1.size-1, r3.size)
+        assertEquals(input.toSet(), (r1+r2+r3).toSet())
+        assertEquals(listOf(1), r1)
+    }
+
+    @Test fun oneTooMany() {
+        val input = listOf(1)
+        val (r1, r2, r3) = nash3(input, input, input)
+        assertEquals(r1.size-1, r2.size)
+        assertEquals(r1.size-1, r3.size)
+        assertEquals(input.toSet(), (r1+r2+r3).toSet())
+        assertEquals(listOf(1), r1)
+    }
+}
