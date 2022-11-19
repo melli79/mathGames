34461f855eebbfe6ac098b03fc8627e94ee3551a
melli <melchiorG@gMail.com>
2022-11-19 17:47:53 +0100

    dp for Knapsack.

---
+++ src/main/kotlin/dynamic/knapsack.kt
@@ -0,0 +1,36 @@
+package dynamic
+
+import common.Quadruple
+
+fun computeKnapsack(items :List<Pair<UInt, UInt>>, size :UInt) :Set<UInt> {
+    val result = mutableSetOf<UInt>()
+    var value = 0u
+    val input = items.mapIndexed { i, p -> Triple(p.first, p.second, i.toUInt()) }
+    input.filter { t -> t.second==0u }.forEach {t ->
+        result.add(t.third)
+        value += t.first
+    }
+    val options = input.filter { t -> t.second!=0u }
+        .map { t -> Quadruple(t.first.toDouble()/t.second.toDouble(), t.first, t.second, t.third) }
+        .toMutableList()
+    return result +bestChoices(size, options).second
+}
+
+private fun bestChoices(size :UInt, options :List<Quadruple<Double, UInt, UInt, UInt>>) :Pair<UInt, Set<UInt>> {
+    val bestOptions = options.filter { q -> q.third <= size }
+        .sortedWith(Comparator
+            .comparing<Quadruple<Double, UInt, UInt, UInt>, Double> { -it.first }
+            .thenComparing<Int> { -it.second.toInt() })
+    if (bestOptions.isNotEmpty()) {
+        val choice = bestOptions.first()
+        val remainingOptions = bestOptions -setOf(choice)
+        val alternative = bestChoices(size, remainingOptions)
+        val rest = bestChoices(size -choice.third, remainingOptions)
+        val value = choice.second + rest.first
+        if (value >= alternative.first) {
+            return Pair(value, rest.second + setOf(choice.fourth))
+        }
+        return alternative
+    }
+    return Pair(0u, emptySet())
+}
---
+++ src/test/kotlin/dynamic/KnapsackTester.kt
@@ -0,0 +1,29 @@
+package dynamic
+
+import kotlin.test.*
+
+class KnapsackTester {
+    @Test fun dontOverload() {
+        val result = computeKnapsack(listOf(Pair(1u,2u), Pair(3u,4u)), 0u)
+
+        assertEquals(emptySet(), result)
+    }
+
+    @Test fun takingFreeItems() {
+        val result = computeKnapsack(listOf(Pair(1u, 0u)), 0u)
+
+        assertEquals(setOf(0u), result)
+    }
+
+    @Test fun bestItem() {
+        val result = computeKnapsack(listOf(Pair(2u, 1u), Pair(3u, 1u)), 1u)
+
+        assertEquals(setOf(1u), result)
+    }
+
+    @Test fun fullestKnapsack() {
+        val result = computeKnapsack(listOf(Pair(3u,3u),Pair(2u,2u),Pair(2u,2u)), 4u)
+
+        assertEquals(setOf(1u,2u), result)
+    }
+}
