f9cfafbe0b26a05edcac0fa73ecb215db56b3453
melli <melchiorG@gMail.com>
2023-03-06 22:05:06 +0100

    fix: organics +symbolic formulas.

--- src/main/kotlin/chemistry/Organics.kt
+++ src/main/kotlin/chemistry/Organics.kt
@@ -1,5 +1,8 @@
 package chemistry
 
+import kotlin.math.max
+
+@OptIn(ExperimentalUnsignedTypes::class)
 data class OrganicMolecule(val clength :UByte, val type :BaseType =BaseType.Alkan, val moieties :List<PositionedMoiety> =emptyList()) {
     companion object {
         val names = listOf("hydrogen", "Methane", "Ethane", "Propane", "Butane", "Pentane",
@@ -7,10 +10,39 @@ data class OrganicMolecule(val clength :UByte, val type :BaseType =BaseType.Alka
     }
 
     override fun toString() :String {
-        val prefix = moieties.joinToString("-") +if (moieties.isNotEmpty()) "-" else ""
+        if (clength==1.toUByte()) {
+            val numH = (4 -moieties.sumOf { pm ->
+                val valence = if (pm.moiety== Moiety.Oxygen) 2  else 1
+                val num = pm.positions.size
+                if (num<1) valence  else num*valence
+            }).toUByte()
+            return Nonmetal.Carbon.symbol + (if (numH>0u) Nonmetal.Hydrogen.symbol+ amount(numH)  else "") +
+                    moieties.joinToString("")
+        }
+        val moieties :Map<UByte, List<Moiety>> = moieties.flatMap { pm ->
+            if (pm.positions.isEmpty()) listOf(Pair(1.toUByte(), pm.moiety))
+            else pm.positions.map { p :UByte -> Pair(p, pm.moiety) }
+        }.groupBy { p -> p.first }.entries.associate { (k :UByte, ps) -> Pair(k, ps.map { it.second }) }
+
+        return (1u..max(clength.toUInt(), moieties.keys.maxOfOrNull { p :UByte -> p.toUInt() } ?: 0u))
+            .joinToString("- ") { p ->
+                val ms :Map<Moiety, List<Moiety>> = moieties.getOrDefault(p.toUByte(), emptyList()).groupBy { it }
+                val covalence = ms.entries.sumOf { it.value.size * if (it.key==Moiety.Oxygen) 2  else 1 }
+                val numH = if (p==1u||p==clength.toUInt()) 3- covalence
+                    else if (p<clength.toUInt()) 2- covalence  else 0
+                (if (p<=clength.toUInt()) Nonmetal.Carbon.symbol else "") +
+                    (if (numH>0) Nonmetal.Hydrogen.symbol+ amount(numH.toUByte()) else "") +
+                    ms.entries.joinToString("") { en ->
+                        en.key.toString()+ amount(en.value.size.toUByte())
+                    }
+            }
+    }
+
+    fun getIupacName() :String {
+        val prefix = moieties.joinToString("-"){ it.getIupacName() } +if (moieties.isNotEmpty()) "-" else ""
         val coreName = if (clength.toInt() < names.size) names[clength.toInt()] else "$clength-alkane"
-        return prefix+when (type) {
-            BaseType.Alkan -> coreName
+        return when (type) {
+            BaseType.Alkan -> prefix+coreName
             BaseType.Acid -> coreName +BaseType.Acid.suffix
             else -> coreName.substring(0 until coreName.length-1) + type.suffix
         }
@@ -22,13 +54,19 @@ data class OrganicMolecule(val clength :UByte, val type :BaseType =BaseType.Alka
     }
 }
 
-data class PositionedMoiety(val moiety :Moiety, val positions :Array<UByte>) {
-    constructor(moiety :Moiety, pos :UByte) :this(moiety, arrayOf(pos))
+@OptIn(ExperimentalUnsignedTypes::class)
+data class PositionedMoiety(val moiety :Moiety, val positions :UByteArray =ubyteArrayOf()) {
+    constructor(moiety :Moiety, pos :UByte) :this(moiety, ubyteArrayOf(pos))
 
     override fun toString() = when (positions.size) {
-        0 -> moiety.toString()
-        1 -> "${positions[0]}-$moiety"
-        else -> "(${positions.joinToString(","){ "$it-" }})-${count(positions.size.toUByte())}$moiety"
+        0, 1 -> moiety.toString()
+        else -> "(${moiety})_${positions.size} "
+    }
+
+    fun getIupacName() = when (positions.size) {
+        0 -> moiety.getIupacName()
+        1 -> "${positions[0]}-${moiety.getIupacName()}"
+        else -> "(${positions.joinToString(","){ "$it-" }})-${count(positions.size.toUByte())}${moiety.getIupacName()}"
     }
 
     override fun equals(other :Any?) :Boolean {
@@ -40,27 +78,56 @@ data class PositionedMoiety(val moiety :Moiety, val positions :Array<UByte>) {
     override fun hashCode() = 31* moiety.hashCode() +positions.contentHashCode()
 }
 
+fun createOrganicMolecule(clength :UByte, type :OrganicMolecule.BaseType) = when(type) {
+    OrganicMolecule.BaseType.Alkan -> OrganicMolecule(clength, type)
+    OrganicMolecule.BaseType.Alcohol -> OrganicMolecule(clength, type, listOf(PositionedMoiety(Moiety.Alcohol, clength)))
+    OrganicMolecule.BaseType.Alcaloid -> OrganicMolecule(clength, type, listOf(PositionedMoiety(Moiety.Oxygen, clength)))
+    OrganicMolecule.BaseType.Acid -> OrganicMolecule(clength, type, listOf(PositionedMoiety(Moiety.Oxygen, clength), PositionedMoiety(Moiety.Alcohol, clength)))
+}
+
 sealed interface Moiety {
-    data class Organic(val clength :UByte) :Moiety {
-        override fun toString() :String {
+    fun getIupacName() :String
+
+    data class Alkyl(val clength :UByte) :Moiety {
+        override fun getIupacName() :String {
             val name = OrganicMolecule.names[clength.toInt()]
             return name.substring(0 until name.length-3) +"yl"
+        }
+
+        override fun toString() = when (clength.toInt()) {
+            0 -> ""
+            1 -> Nonmetal.Carbon.symbol + Nonmetal.Hydrogen.symbol +"_3 "
+            else -> "("+ (2u..clength.toUInt()).joinToString("-") {
+                        Nonmetal.Carbon.symbol + Nonmetal.Hydrogen.symbol +"_2"
+                    } +"-"+ Nonmetal.Carbon.symbol + Nonmetal.Hydrogen.symbol +"_3" +")"
         }
     }
 
     object Amine :Moiety {
-        override fun toString() = "amine"
+        override fun getIupacName() = "amine"
+
+        override fun toString() = Nonmetal.Nitrogen.symbol + Nonmetal.Hydrogen.symbol +"_2 "
     }
 
     object Alcohol :Moiety {
-        override fun toString() = "ol"
+        override fun getIupacName() = "ol"
+
+        override fun toString() = Nonmetal.Oxygen.symbol + Nonmetal.Hydrogen.symbol
+    }
+
+    object Oxygen :Moiety {
+        override fun getIupacName() = "al"
+
+        override fun toString() = Nonmetal.Oxygen.symbol
     }
 
     data class Inorganic1(val atom :Nonmetal) :Moiety {
-        override fun toString() :String {
-            val name = atom.toString()
+        override fun getIupacName() :String {
+            val name = atom.name
             return name.substring(0 until name.length-3) +"o"
         }
+
+        override fun toString() = atom.symbol
     }
 
     class Inorganic2(element1 :Atom, element2 :Atom, amount1 :UByte, amount2 :UByte) :Moiety,
--- src/test/kotlin/chemistry/OrganicsTester.kt
+++ src/test/kotlin/chemistry/OrganicsTester.kt
@@ -2,53 +2,58 @@ package chemistry
 
 import kotlin.test.*
 
+@OptIn(ExperimentalUnsignedTypes::class)
 class OrganicsTester {
     @Test fun alkanes() {
         for (n in 1..15) {
-            println(OrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Alkan))
+            val alkan = OrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Alkan)
+            println("${alkan.getIupacName()}: $alkan")
         }
     }
 
     @Test fun Alcanoles() {
         for (n in 1..10) {
-            print(OrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Alcohol))
-            println(", "+ OrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Alcaloid))
+            val alkanol = createOrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Alcohol)
+            println("${alkanol.getIupacName()}: $alkanol,")
+            val alkanal = createOrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Alcaloid)
+            println("  ${alkanal.getIupacName()}: $alkanal")
         }
     }
 
     @Test fun Acids() {
         for (n in 1..10) {
-            println(OrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Acid))
+            val acid = createOrganicMolecule(n.toUByte(), OrganicMolecule.BaseType.Acid)
+            println("${acid.getIupacName()}: $acid")
         }
     }
 
     @Test fun polyAlcohol() {
         val propantriol = OrganicMolecule(3u, moieties= listOf(
-            PositionedMoiety(Moiety.Alcohol, arrayOf(1u, 2u, 3u) )))
-        println(propantriol)
+            PositionedMoiety(Moiety.Alcohol, ubyteArrayOf(1u, 2u, 3u) )))
+        println("${propantriol.getIupacName()}: $propantriol")
     }
 
     @Test fun amines() {
         val diamineHexan = OrganicMolecule(6u,
             moieties= listOf(
-                PositionedMoiety(Moiety.Amine, arrayOf(2u, 4u))
+                PositionedMoiety(Moiety.Amine, ubyteArrayOf(2u, 4u))
             ))
-        println(diamineHexan)
+        println("${diamineHexan.getIupacName()}: $diamineHexan")
     }
 
     @Test fun halogeneAlkan() {
         val fckw = OrganicMolecule(1u, moieties= listOf(
-            PositionedMoiety(Moiety.Inorganic1(Nonmetal.Chlorine), arrayOf(2u,3u,4u)),
-            PositionedMoiety(Moiety.Inorganic1(Nonmetal.Fluorine), 1u)
+            PositionedMoiety(Moiety.Inorganic1(Nonmetal.Chlorine), ubyteArrayOf(1u,1u,1u)),
+            PositionedMoiety(Moiety.Inorganic1(Nonmetal.Fluorine))
         ))
-        println(fckw)
+        println(fckw.getIupacName()+": $fckw")
     }
 
     @Test fun polyalcane() {
         val methylEthylHexane = OrganicMolecule(6u, moieties= listOf(
-            PositionedMoiety(Moiety.Organic(1u), 2u),
-            PositionedMoiety(Moiety.Organic(2u), 4u),
+            PositionedMoiety(Moiety.Alkyl(1u), 2u),
+            PositionedMoiety(Moiety.Alkyl(2u), 4u),
         ))
-        println(methylEthylHexane)
+        println(methylEthylHexane.getIupacName()+": $methylEthylHexane")
     }
 }
