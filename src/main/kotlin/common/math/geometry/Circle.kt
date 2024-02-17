e28da39de787e6696e4234cf038584cbe1aced51
Melchior <melchiorG@gMail.com>
2024-02-17 22:38:23 +0100

    intersection of 2 circles.

--- src/main/kotlin/common/math/geometry/Circle.kt
+++ src/main/kotlin/common/math/geometry/Circle.kt
@@ -2,6 +2,7 @@ package common.math.geometry
 
 import common.math.epsilon
 import common.math.sqr
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.sqrt
@@ -14,12 +15,11 @@ data class Circle(val center :Point2D, val radius :Double) {
             return emptySet()
         val l = (sqr(radius) - sqr(c2.radius)) / d
         val h2 = sqr(radius) - sqr(l)
-        if (h2 < 3 * epsilon * radius * l)
+        if (h2 < 3 * epsilon *(radius*abs(l) +1.0))
             return setOf(center.translate(v.normed() * l))
         val e = v*(1/d)
         val b = center.translate(e * l)
         val p = e.perp() * sqrt(h2)
         return setOf(b.translate(p), b.translate(-p))
     }
-
 }
